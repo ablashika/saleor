7db45088d430576585baecbb7ca4dce4e49431e6
ablashika <kofoworola203@gmail.com>
2023-12-21 00:15:27 +0000

    added changes

--- src/components/FeatureBox.js
+++ src/components/FeatureBox.js
@@ -7,13 +7,28 @@ import {
   faStore, faCalendarCheck, faEnvelopeOpenText, faCheckSquare, faCashRegister,
 } from '@fortawesome/free-solid-svg-icons';
 
+import { motion, useAnimation } from 'framer-motion';
+
 const FeatureBox = ({ icon, title }) => (
-  <Col md={4}>
-    <div className="feature-box">
+  <Col md={4} style={{marginLeft:"70px"}}>
+     <motion.div
+      style={{
+        border: '1px solid black',
+        borderRadius: '50px',
+        padding: '15px',
+        backgroundColor: '#fffff',
+        width: '400px',
+        display: 'flex',
+        justifyContent: 'space-around',
+        marginLeft: '630px '
+      }}
+      animate={{ x: '100%', transition: { duration: 10, repeat: Infinity, repeatType: 'loop' } }}
+      whileHover={{ scale: 1.05, transition: { duration: 20 } }}
+     
+      >
       <FontAwesomeIcon icon={icon} size="2x" />
       <h3>{title}</h3>
-      <p>Feature description goes here.</p>
-    </div>
+    </motion.div>
   </Col>
 );
 
@@ -34,16 +49,49 @@ const FeatureBoxDetails = () => {
     { icon: faCalendarCheck, title: "Reservations" },
     { icon: faEnvelopeOpenText, title: "Pay by invoice" },
     { icon: faCheckSquare, title: "Express checkout" },
-    { icon: faCashRegister, title: "Cash on delivery" },
+    // { icon: faCashRegister, title: "Cash on delivery" },
+    // { icon: faSync, title: "Subscriptions" },
+    // { icon: faMapMarkerAlt, title: "Address validation" },
+    // { icon: faFileAlt, title: "Order quotes" },
+    // { icon: faMoneyBill, title: "Pay later" },
+    // { icon: faTruck, title: "Multi-fulfillment" },
+    // { icon: faCreditCard, title: "Split payments" },
+    // { icon: faFileInvoiceDollar, title: "Single page" },
+    // { icon: faBoxes, title: "Multi-gateway" },
+    // { icon: faFileSignature, title: "Pre-orders" },
+    // { icon: faHandshake, title: "Order approvals" },
+    // { icon: faCommentDollar, title: "Custom payment" },
+    // { icon: faStore, title: "Pick-up in store" },
+    // { icon: faCalendarCheck, title: "Reservations" },
+    // { icon: faEnvelopeOpenText, title: "Pay by invoice" },
+    // { icon: faCheckSquare, title: "Express checkout" },
+    // { icon: faCashRegister, title: "Cash on delivery" },
   ];
 
   return (
     <Container>
-      <Row>
-        {features.map(({ icon, title }, index) => (
-          <FeatureBox key={index} icon={icon} title={title} />
-        ))}
+      <Row className='feature-container' >
+   
+        <motion.div
+          className=''
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            overflowX:"scroll",
+            margin:'20px',
+            height: '150px',
+            whiteSpace: 'nowrap',
+            flexWrap:"initial"
+           
+          }}
+             >
+          {features.map(({ icon, title }, index) => (
+            <FeatureBox key={index} icon={icon} title={title} />
+          ))}
+        </motion.div>
       </Row>
+  
     </Container>
   );
 };

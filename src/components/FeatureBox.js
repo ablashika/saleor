f64a0d662269707f4e74ee866805853cfa6e5232
ablashika <kofoworola203@gmail.com>
2023-12-21 00:34:04 +0000

    maded changes

--- src/components/FeatureBox.js
+++ src/components/FeatureBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -7,10 +7,10 @@ import {
   faStore, faCalendarCheck, faEnvelopeOpenText, faCheckSquare, faCashRegister,
 } from '@fortawesome/free-solid-svg-icons';
 
-import { motion, useAnimation } from 'framer-motion';
+import { motion,useAnimation, useMotionValue} from 'framer-motion';
 
 const FeatureBox = ({ icon, title }) => (
-  <Col md={4} style={{marginLeft:"70px"}}>
+  <Col md={4}  style={{marginLeft:"70px"}}>
      <motion.div
       style={{
         border: '1px solid black',
@@ -20,12 +20,18 @@ const FeatureBox = ({ icon, title }) => (
         width: '400px',
         display: 'flex',
         justifyContent: 'space-around',
-        marginLeft: '630px '
+    
+      }} 
+
+
+      animate={{
+        x: [-400, 400],
+        transition: { duration: 10, ease:'linear', repeat: Infinity, repeatType: 'loop' },
       }}
-      animate={{ x: '100%', transition: { duration: 10, repeat: Infinity, repeatType: 'loop' } }}
-      whileHover={{ scale: 1.05, transition: { duration: 20 } }}
-     
-      >
+      whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+
+      
+           >
       <FontAwesomeIcon icon={icon} size="2x" />
       <h3>{title}</h3>
     </motion.div>
@@ -34,6 +40,12 @@ const FeatureBox = ({ icon, title }) => (
 
 const FeatureBoxDetails = () => {
   const features = [
+    { icon: faSync, title: "Subscriptions" },
+    { icon: faMapMarkerAlt, title: "Address validation" },
+    { icon: faFileAlt, title: "Order quotes" },
+    { icon: faMoneyBill, title: "Pay later" },
+    { icon: faTruck, title: "Multi-fulfillment" },
+     { icon: faCashRegister, title: "Cash on delivery" },
     { icon: faSync, title: "Subscriptions" },
     { icon: faMapMarkerAlt, title: "Address validation" },
     { icon: faFileAlt, title: "Order quotes" },
@@ -49,24 +61,22 @@ const FeatureBoxDetails = () => {
     { icon: faCalendarCheck, title: "Reservations" },
     { icon: faEnvelopeOpenText, title: "Pay by invoice" },
     { icon: faCheckSquare, title: "Express checkout" },
-    // { icon: faCashRegister, title: "Cash on delivery" },
-    // { icon: faSync, title: "Subscriptions" },
-    // { icon: faMapMarkerAlt, title: "Address validation" },
-    // { icon: faFileAlt, title: "Order quotes" },
-    // { icon: faMoneyBill, title: "Pay later" },
-    // { icon: faTruck, title: "Multi-fulfillment" },
-    // { icon: faCreditCard, title: "Split payments" },
-    // { icon: faFileInvoiceDollar, title: "Single page" },
-    // { icon: faBoxes, title: "Multi-gateway" },
-    // { icon: faFileSignature, title: "Pre-orders" },
-    // { icon: faHandshake, title: "Order approvals" },
-    // { icon: faCommentDollar, title: "Custom payment" },
-    // { icon: faStore, title: "Pick-up in store" },
-    // { icon: faCalendarCheck, title: "Reservations" },
-    // { icon: faEnvelopeOpenText, title: "Pay by invoice" },
-    // { icon: faCheckSquare, title: "Express checkout" },
-    // { icon: faCashRegister, title: "Cash on delivery" },
+    { icon: faCashRegister, title: "Cash on delivery" },
   ];
+
+
+  const scrollY = useMotionValue(0);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const updateScroll = () => {
+      scrollY.set(window.scrollY);
+      controls.start({ x: [-400, 400] }); // Adjust the animation values
+    };
+
+    window.addEventListener('scroll', updateScroll);
+    return () => window.removeEventListener('scroll', updateScroll);
+  }, [scrollY, controls]);
 
   return (
     <Container>
@@ -77,17 +87,17 @@ const FeatureBoxDetails = () => {
           style={{
             display: 'flex',
             flexDirection: 'row',
-            justifyContent: 'space-around',
-            overflowX:"scroll",
-            margin:'20px',
             height: '150px',
+            width:'100%',
             whiteSpace: 'nowrap',
-            flexWrap:"initial"
+            
+            
+        
            
           }}
              >
           {features.map(({ icon, title }, index) => (
-            <FeatureBox key={index} icon={icon} title={title} />
+            <FeatureBox key={index} icon={icon} title={title} control={controls}  />
           ))}
         </motion.div>
       </Row>
